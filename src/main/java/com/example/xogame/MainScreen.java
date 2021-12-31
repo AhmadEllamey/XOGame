df01bef24f4b2ac59738bd3fb1b8de71448e6e6c
ahmad <ahmadellamey1@gmail.com>
2021-12-31 21:54:23 +0200

    Second Version

---
+++ src/main/java/com/example/xogame/MainScreen.java
@@ -0,0 +1,124 @@
+package com.example.xogame;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+public class MainScreen {
+
+
+    @FXML
+    private JFXButton playWithFriendButton;
+
+    @FXML
+    private JFXButton playWithPCButton;
+
+    @FXML
+    private JFXButton playOnlineButton;
+
+    @FXML
+    private JFXButton exitButton;
+
+    @FXML
+    private ListView<?> onlineUsersList;
+
+    @FXML
+    private Label playerOneNameLabel;
+
+    @FXML
+    private Label playerOneScoreLabel;
+
+    @FXML
+    private Label playerTwoNameLabel;
+
+    @FXML
+    private Label playerTwoScoreLabel;
+
+    @FXML
+    private Pane P_0_0;
+
+    @FXML
+    private Pane P_0_1;
+
+    @FXML
+    private Pane P_0_2;
+
+    @FXML
+    private Pane P_1_0;
+
+    @FXML
+    private Pane P_1_1;
+
+    @FXML
+    private Pane P_1_2;
+
+    @FXML
+    private Pane P_2_0;
+
+    @FXML
+    private Pane P_2_1;
+
+    @FXML
+    private Pane P_2_2;
+
+    @FXML
+    private JFXButton recordGameButton;
+
+    @FXML
+    private JFXButton surrenderButton;
+
+    @FXML
+    private ImageView userImageView;
+
+    @FXML
+    private Label usernameLabel;
+
+    @FXML
+    void manageExitButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void managePlayOnlineButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void managePlayWithFriendButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void managePlayWithPCButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void manageRecordGameButton(ActionEvent event) {
+
+
+
+    }
+
+    @FXML
+    void manageSurrenderButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void manageTheGameBoard(MouseEvent event) {
+
+    }
+
+
+
+
+
+
+
+}
