7ac39a32869f6673a9b2d29795e88a43065a4234
muhammad_kh <=>
2021-12-31 23:45:18 +0200

    Winning Added

---
+++ src/main/java/com/example/xogame/WinningScreen.java
@@ -0,0 +1,37 @@
+package com.example.xogame;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class WinningScreen {
+
+    @FXML
+    private JFXButton backToHome;
+
+    @FXML
+    private JFXButton exit;
+
+    @FXML
+    private JFXButton playAgain;
+
+    @FXML
+    private Label score;
+
+    @FXML
+    void manageBackToHome(MouseEvent event) {
+
+    }
+
+    @FXML
+    void manageExit(MouseEvent event) {
+
+    }
+
+    @FXML
+    void managePlayAgain(MouseEvent event) {
+
+    }
+
+}
