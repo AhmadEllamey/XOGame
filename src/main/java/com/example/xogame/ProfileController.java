fe5f724c9a3ad9ae15dca1d4f7d994d7a6196e07
mariam <mariammostafa7799@gmail.com>
2021-12-31 23:34:27 +0200

    profile

---
+++ src/main/java/com/example/xogame/ProfileController.java
@@ -0,0 +1,24 @@
+package com.example.xogame;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+public class ProfileController {
+    @FXML
+    private JFXTextField phoneTxt;
+
+    @FXML
+    private Label totalGameTxt;
+
+    @FXML
+    private JFXButton updatebtn;
+
+    @FXML
+    private JFXTextField userNameTxt;
+
+    @FXML
+    void updatebtnClick(ActionEvent event) {
+
+    }
+}
