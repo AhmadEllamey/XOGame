0d2b327838acffb7aa1e404d4c97bd952621a26d
Shrouk Mamdouh <ShroukMamdouh_127@yahoo.com>
2022-01-03 15:18:44 +0200

    Shrouk Task difficulty level

---
+++ src/main/java/com/example/xogame/DifficultyLevel.java
@@ -0,0 +1,44 @@
+package com.example.xogame;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class DifficultyLevel {
+    @FXML
+    private Button CancelButton;
+
+    @FXML
+    private Text DifficultyLevel;
+
+    @FXML
+    private RadioButton HardLevel;
+
+    @FXML
+    private Button OkButton;
+
+    @FXML
+    private ToggleGroup difficultyGroup;
+
+    @FXML
+    private RadioButton moderateLevel;
+
+    @FXML
+    private RadioButton simpleLevel;
+
+    @FXML
+    void manageCancelButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void manageOkButton(MouseEvent event) {
+
+    }
+
+}
+
+
