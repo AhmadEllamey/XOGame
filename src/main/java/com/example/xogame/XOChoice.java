9b363a3274eef31590e144767c2eaa72fc04fdad
muhammad_kh <mohamedkholif710@gmail.com>
2022-01-03 14:05:03 +0200

    XorO Screen files ADDED

---
+++ src/main/java/com/example/xogame/XOChoice.java
@@ -0,0 +1,25 @@
+package com.example.xogame;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class XOChoice {
+
+    @FXML
+    private JFXButton oBtn;
+
+    @FXML
+    private JFXButton xBtn;
+
+    @FXML
+    void handleOBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleXBtn(ActionEvent event) {
+
+    }
+
+}
