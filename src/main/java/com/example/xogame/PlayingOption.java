c339f1f6a16158358f96871a2c1ac009d4cfe759
ahmad <ahmadellamey1@gmail.com>
2022-01-14 18:24:05 +0200

    pre-final Version

--- src/main/java/com/example/xogame/PlayingOption.java
+++ src/main/java/com/example/xogame/PlayingOption.java
@@ -1,19 +1,18 @@
 package com.example.xogame;
 
 import com.jfoenix.controls.JFXButton;
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
+
+import java.io.IOException;
 import java.net.URL;
-import java.util.Optional;
+
 import java.util.ResourceBundle;
 
 public class PlayingOption implements Initializable {
@@ -54,11 +53,23 @@ public class PlayingOption implements Initializable {
     @FXML
     void playWithComputer(MouseEvent event) {
 
+
+        // ToDO mohamed will type his code
+
     }
 
     @FXML
     void playWithFriend(MouseEvent event) {
-
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("PlayersNameScreen.fxml"));
+        try {
+            Parent profileScreen = loader.load();
+            Stage stage=new Stage();
+            stage.setScene(new Scene(profileScreen,510,285));
+            stage.show();
+            PlayersNameController.setStage(stage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
